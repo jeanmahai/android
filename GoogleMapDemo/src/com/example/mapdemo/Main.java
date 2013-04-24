957cb239e4cf423f7a58f2c2fa964a44d76101b6
jeanmahai <mahai_1986@126.com>
2013-04-24 11:15:22 +0800

    add google map v2 demo

---
+++ GoogleMapDemo/src/com/example/mapdemo/Main.java
@@ -0,0 +1,17 @@
+package com.example.mapdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.mytree.GoogleMapDemo.R;
+
+public class Main extends Activity  {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
